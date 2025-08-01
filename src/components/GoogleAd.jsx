d72930c07625576876e3a2447f7225be60e5123f
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-08-01 18:10:14 -0300

    Update GoogleAd.jsx

--- src/components/GoogleAd.jsx
+++ src/components/GoogleAd.jsx
@@ -6,7 +6,6 @@ export default function GoogleAd() {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
     } catch (e) {
       // Ignora erro caso o anúncio já tenha sido carregado
-      // console.log(e);
     }
   }, []);
 
@@ -15,7 +14,7 @@ export default function GoogleAd() {
       className="adsbygoogle"
       style={{ display: "block" }}
       data-ad-format="fluid"
-      data-ad-layout-key="-6q+e9+15-2u+4y"
+      data-ad-layout-key="-f6+2m+15-2s+4x"
       data-ad-client="ca-pub-2315578510011966"
       data-ad-slot="4351180023"
     ></ins>
